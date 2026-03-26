770bc294cab8036c39870d7fb5fad1103c6669aa
mariamaryan008 <maria.mar.yan008@gmail.com>
2026-03-26 11:18:18 +0400

    Add files via upload

---
+++ MyStack/MyStack.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyLinkedListProgramm
+{
+    public class MyStack<T>
+    {
+        private MyLinkedList<T> list;
+
+        public MyStack()
+        {
+            list = new MyLinkedList<T>();
+        }
+
+        public void Push(T item)
+        {
+            list.AddFirst(item);
+        }
+
+        public T Pop()
+        {
+            if (list.Count == 0)
+                throw new InvalidOperationException("Stack is empty.");
+
+            T value = list.Head.Value;
+            list.RemoveFirst(value);
+            return value;
+        }
+
+        public T Peek()
+        {
+            if (list.Count == 0)
+                throw new InvalidOperationException("Stack is empty.");
+
+            return list.Head.Value;
+        }
+    }
+}
