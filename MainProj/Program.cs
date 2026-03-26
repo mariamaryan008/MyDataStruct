9df095c5ac1abe5805378ca00ce4c1187eb423a2
mariamaryan008 <maria.mar.yan008@gmail.com>
2026-03-26 11:20:10 +0400

    Add files via upload

---
+++ MainProj/Program.cs
@@ -0,0 +1,78 @@
+﻿using MyBinaryTree;
+using MyLinkedListProgramm;
+
+namespace MainProj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack<int> stack = new MyStack<int>();
+            MyQueue<int> queue = new MyQueue<int>();   
+            stack.Push(1);
+            stack.Push(2);  
+            stack.Push(3);
+            Console.WriteLine(stack);
+            Console.WriteLine(stack.Peek());
+            Console.WriteLine(stack.Pop());
+
+            queue.Enqueue(1);
+            queue.Enqueue(2);   
+            queue.Enqueue(3);
+
+            Console.WriteLine(queue);
+
+            Console.WriteLine(queue.Peek());
+            Console.WriteLine(queue.Dequeue());
+
+
+            MyBinaryTree<int> tree = new MyBinaryTree<int>();
+
+            // Add
+            tree.Add(5);
+            tree.Add(3);
+            tree.Add(8);
+            tree.Add(9);
+            tree.Add(2);
+            tree.Add(6);
+            tree.Add(7);
+
+            //In-Order 
+            foreach (var item in tree.InOrder())
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine( );
+            //Pre-Order 
+            foreach (var item in tree.PreOrder())
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            //Post-Order Traversal
+            foreach (var item in tree.PostOrder())
+            {
+                Console.Write(item + " ");
+            }
+
+            // Remove
+            tree.Remove(2);
+
+            tree.Remove(3);
+
+            tree.Remove(5);
+
+            Console.WriteLine();
+
+            //PreOrder After Removals
+            foreach (var item in tree.PreOrder())
+            {
+                Console.Write(item + " ");
+            }
+
+
+        }
+    }
+}
---
+++ MyBinaryTree/MyBinaryTree.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MyBinaryTree;
+
+public class MyBinaryTree<T> : IEnumerable<T>
+    where T : IComparable<T>
+{
+    private MyBinaryTreeNode<T> Root { get; set; }
+    private int Count;
+
+    #region Find
+    private MyBinaryTreeNode<T> Find(T value)
+    {
+        return Find(Root, value);
+    }
+    private MyBinaryTreeNode<T> Find(MyBinaryTreeNode<T> node, T value)
+    {
+        if (node == null)
+            return null;
+        int compare = value.CompareTo(node.Value);
+        if (compare == 0)
+            return node;
+        else if (compare < 0)
+            return Find(node.Left, value);
+
+        return Find(node.Right, value);
+
+    }
+
+    #endregion
+
+    #region Add
+
+    public void Add(T value)
+    {
+        Root = Add(Root, value);
+        Count++;
+    }
+
+    private static MyBinaryTreeNode<T> Add(MyBinaryTreeNode<T> node, T value)
+    {
+        if (node == null)
+            return new MyBinaryTreeNode<T>(value);
+
+        if (value.CompareTo(node.Value) < 0)
+            node.Left = Add(node.Left, value);
+        else if (value.CompareTo(node.Value) > 0)
+            node.Right = Add(node.Right, value);
+
+        return node;
+    }
+
+    #endregion
+
+    #region Remove
+
+    public void Remove(T value)
+    {
+        Root = Remove(Root, value);
+    }
+
+    private MyBinaryTreeNode<T> Remove(MyBinaryTreeNode<T> node, T value)
+    {
+        if (node == null)
+            return null;
+
+        int compare = value.CompareTo(node.Value);
+
+        if (compare < 0)
+            node.Left = Remove(node.Left, value);
+        else if (compare > 0)
+            node.Right = Remove(node.Right, value);
+        else
+        {
+            // No children
+            if (node.Left == null && node.Right == null)
+            {
+                Count--;
+                return null;
+            }
+
+            // One child
+            if (node.Left == null)
+            {
+                Count--;
+                return node.Right;
+            }
+
+            if (node.Right == null)
+            {
+                Count--;
+                return node.Left;
+            }
+
+            // Two children
+            MyBinaryTreeNode<T> min = FindMin(node.Right);
+            node.Value = min.Value;
+            node.Right = Remove(node.Right, min.Value);
+        }
+
+        return node;
+    }
+
+    private MyBinaryTreeNode<T> FindMin(MyBinaryTreeNode<T> node)
+    {
+        while (node.Left != null)
+            node = node.Left;
+        return node;
+    }
+
+    #endregion
+
+    #region Traversals
+
+    // In-order 
+    public List<T> InOrder()
+    {
+        var result = new List<T>();
+        InOrder(Root, result);
+        return result;
+    }
+
+    private void InOrder(MyBinaryTreeNode<T> node, List<T> result)
+    {
+        if (node == null)
+            return;
+
+        InOrder(node.Left, result);
+        result.Add(node.Value);
+        InOrder(node.Right, result);
+    }
+
+    // Pre-order 
+    public List<T> PreOrder()
+    {
+        var result = new List<T>();
+        PreOrder(Root, result);
+        return result;
+    }
+
+    private void PreOrder(MyBinaryTreeNode<T> node, List<T> result)
+    {
+        if (node == null)
+            return;
+
+        result.Add(node.Value);
+        PreOrder(node.Left, result);
+        PreOrder(node.Right, result);
+    }
+
+    // Post-order
+    public List<T> PostOrder()
+    {
+        var result = new List<T>();
+        PostOrder(Root, result);
+        return result;
+    }
+
+    private void PostOrder(MyBinaryTreeNode<T> node, List<T> result)
+    {
+        if (node == null)
+            return;
+
+        PostOrder(node.Left, result);
+        PostOrder(node.Right, result);
+        result.Add(node.Value);
+    }
+
+    #endregion
+
+    #region IEnumerable
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        return InOrder().GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    #endregion
+}
